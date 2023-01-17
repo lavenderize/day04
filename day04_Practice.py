3f5249ce529843a2a2f309af35620b48ea6ffa35
lavenderize <liexquisit@gmail.com>
2023-01-17 16:31:19 +0900

    #7.9

--- day04_Practice.py
+++ day04_Practice.py
@@ -1,2 +1,9 @@
 # 7.8
 surprise = ["Groucho", "Chico", "Harpo"]
+
+# 7.9
+print(surprise[2].lower())
+print(list(reversed(surprise)))
+# print(surprise.capitalize())
+surprise = [surprise.capitalize() for surprise in surprise]
+print(surprise)
