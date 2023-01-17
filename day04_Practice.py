031ec4afeb32e7af804b95ae9e2987529e769465
lavenderize <liexquisit@gmail.com>
2023-01-17 16:14:34 +0900

    #7.3

--- day04_Practice.py
+++ day04_Practice.py
@@ -8,5 +8,7 @@ for year in range(1, 7):
 print(year_lists)
 
 # 7.2
-print(f'years_list의 세 번째 생일은 {year_lists[4]}년에 맞는다.')
+print(f'years_list의 세 번째 생일은 {year_lists[3]}년에 맞는다.')
 
+# 7.3
+print(f'years_list 중 가장 나이가 많을 때는 {year_lists[-1]}년이다.')
