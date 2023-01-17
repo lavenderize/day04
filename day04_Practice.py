1a4e9291e58c5f4e7f4f2746e702371e837e71a8
lavenderize <liexquisit@gmail.com>
2023-01-17 16:16:52 +0900

    7.8

--- day04_Practice.py
+++ day04_Practice.py
@@ -1,14 +1,2 @@
-# 7.1
-
-input_year = int(input('출생년도를 입력하세요: '))
-
-year_lists = []
-for year in range(1, 7):
-    year_lists.append(year + input_year)
-print(year_lists)
-
-# 7.2
-print(f'years_list의 세 번째 생일은 {year_lists[3]}년에 맞는다.')
-
-# 7.3
-print(f'years_list 중 가장 나이가 많을 때는 {year_lists[-1]}년이다.')
+# 7.8
+surprise = ["Groucho", "Chico", "Harpo"]
