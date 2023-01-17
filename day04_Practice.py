7931399b81a276ac46c4b163b46cc5769567b762
lavenderize <liexquisit@gmail.com>
2023-01-17 15:11:41 +0900

    day04_Examples

--- day04_Practice.py
+++ day04_Practice.py
@@ -87,3 +87,33 @@ for k, v in students.items():
 
 print(f'이름은 {students["name"]}, 나이는 {students["age"]}세,', end=' ')
 print(f'시력은 좌: {students["eyes"][0]}, 우: {students["eyes"][1]}')
+
+# 안주 추천 프로그램
+
+alcohol_foods = {
+    '맥주': '치킨',
+    '소주': '골뱅이 소면',
+    '위스키': '치즈',
+    '고량주': '짬뽕'
+}
+alcohol_list = list(alcohol_foods)
+food_list = [food for food in alcohol_foods.values()]
+print(alcohol_list, food_list)
+
+while True:
+    alcohol = input(f'술을 선택 하세요 1) {alcohol_list[0]} 2) {alcohol_list[1]} 3) {alcohol_list[2]} 4) {alcohol_list[3]} 5) 계산')
+    if alcohol == '5':
+        print('다음에 또 오세요!')
+        break;
+    elif alcohol == '1':
+        print(f'{alcohol_list[0]}에 어울리는 안주는 {food_list[0]} 입니다.')
+    elif alcohol == '2':
+        print(f'{alcohol_list[0]}에 어울리는 안주는 {food_list[0]} 입니다.')
+    elif alcohol == '3':
+        print(f'{alcohol_list[0]}에 어울리는 안주는 {food_list[0]} 입니다.')
+    elif alcohol == '4':
+        print(f'{alcohol_list[0]}에 어울리는 안주는 {food_list[0]} 입니다.')
+    else:
+        print('메뉴에서 골라주세요.')
+
+        # {random.choice(alcohol_list)} : {random.choice(food_list)}
